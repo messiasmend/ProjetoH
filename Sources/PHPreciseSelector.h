cb35e73466079e006d686dd0d2d9e41160f1b321
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-08-23 14:27:41 -0300

    ProjetoH V4: add precise touch selector

---
+++ Sources/PHPreciseSelector.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PHPreciseSelector : NSObject
++ (instancetype)sharedSelector;
+- (void)processEvent:(UIEvent *)event inspectorWindow:(nullable UIWindow *)inspectorWindow;
+@end
+
+NS_ASSUME_NONNULL_END
