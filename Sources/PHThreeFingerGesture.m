b8571499e034b7e1a64e013239bc14f3a0812ef1
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-08-23 14:28:18 -0300

    ProjetoH V4: reset selector when inspection starts

--- Sources/PHThreeFingerGesture.m
+++ Sources/PHThreeFingerGesture.m
@@ -1,5 +1,6 @@
 #import "PHThreeFingerGesture.h"
 #import "PHOverlayManager.h"
+#import "PHPreciseSelector.h"
 
 static const NSUInteger PHRequiredFingerCount = 3;
 static NSTimeInterval const PHThreeFingerHoldInterval = 0.8;
@@ -12,13 +13,9 @@ static NSTimeInterval const PHThreeFingerHoldInterval = 0.8;
 @implementation PHThreeFingerGesture
 
 - (void)processEvent:(UIEvent *)event {
-    if (event == nil || ![event respondsToSelector:@selector(allTouches)]) {
-        return;
-    }
-
+    if (event == nil || ![event respondsToSelector:@selector(allTouches)]) return;
     NSSet<UITouch *> *touches = event.allTouches;
     NSUInteger activeTouches = 0;
-
     for (UITouch *touch in touches) {
         switch (touch.phase) {
             case UITouchPhaseBegan:
@@ -30,68 +27,35 @@ static NSTimeInterval const PHThreeFingerHoldInterval = 0.8;
                 break;
         }
     }
-
-    if (activeTouches >= PHRequiredFingerCount) {
-        [self armIfNeeded];
-    } else {
-        [self cancelHoldAndResetTrigger];
-    }
+    if (activeTouches >= PHRequiredFingerCount) [self armIfNeeded];
+    else [self cancelHoldAndResetTrigger];
 }
 
 - (void)armIfNeeded {
-    if (self.triggered || self.holdTimer != nil) {
-        return;
-    }
-
-    if (![NSThread isMainThread]) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self armIfNeeded];
-        });
-        return;
-    }
-
+    if (self.triggered || self.holdTimer != nil) return;
+    if (![NSThread isMainThread]) { dispatch_async(dispatch_get_main_queue(), ^{ [self armIfNeeded]; }); return; }
     __weak typeof(self) weakSelf = self;
-    self.holdTimer = [NSTimer timerWithTimeInterval:PHThreeFingerHoldInterval
-                                              repeats:NO
-                                                block:^(__unused NSTimer *timer) {
+    self.holdTimer = [NSTimer timerWithTimeInterval:PHThreeFingerHoldInterval repeats:NO block:^(__unused NSTimer *timer) {
         __strong typeof(weakSelf) strongSelf = weakSelf;
         [strongSelf handleHoldTimerFired];
     }];
-
-    [[NSRunLoop mainRunLoop] addTimer:self.holdTimer
-                              forMode:NSRunLoopCommonModes];
+    [[NSRunLoop mainRunLoop] addTimer:self.holdTimer forMode:NSRunLoopCommonModes];
 }
 
 - (void)handleHoldTimerFired {
     self.holdTimer = nil;
-
-    if (self.triggered) {
-        return;
-    }
-
+    if (self.triggered) return;
     self.triggered = YES;
+    [[PHPreciseSelector sharedSelector] reset];
     [[PHOverlayManager sharedManager] startSelectionMode];
 }
 
 - (void)cancelHoldAndResetTrigger {
-    if (![NSThread isMainThread]) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self cancelHoldAndResetTrigger];
-        });
-        return;
-    }
-
+    if (![NSThread isMainThread]) { dispatch_async(dispatch_get_main_queue(), ^{ [self cancelHoldAndResetTrigger]; }); return; }
     [self cancelTimer];
     self.triggered = NO;
 }
 
-- (void)cancelTimer {
-    [self.holdTimer invalidate];
-    self.holdTimer = nil;
-}
-
-- (void)reset {
-    [self cancelHoldAndResetTrigger];
-}
-
+- (void)cancelTimer { [self.holdTimer invalidate]; self.holdTimer = nil; }
+- (void)reset { [self cancelHoldAndResetTrigger]; }
 @end
