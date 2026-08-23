c86f921f92c28e8888ed484d11b2dcfbe7cb785c
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-08-23 12:16:09 -0300

    Implement initial three-finger hold state machine

---
+++ Sources/PHThreeFingerGesture.m
@@ -0,0 +1,86 @@
+#import "PHThreeFingerGesture.h"
+#import "PHOverlayManager.h"
+
+static NSTimeInterval const PHThreeFingerHoldInterval = 0.8;
+
+@interface PHThreeFingerGesture ()
+@property (nonatomic, strong, nullable) NSTimer *holdTimer;
+@property (nonatomic, assign) BOOL triggered;
+@end
+
+@implementation PHThreeFingerGesture
+
+- (void)processEvent:(UIEvent *)event {
+    if (![event respondsToSelector:@selector(allTouches)]) {
+        return;
+    }
+
+    NSSet<UITouch *> *touches = event.allTouches;
+    NSUInteger qualifyingTouches = 0;
+
+    for (UITouch *touch in touches) {
+        UITouchPhase phase = touch.phase;
+        if (phase == UITouchPhaseBegan ||
+            phase == UITouchPhaseMoved ||
+            phase == UITouchPhaseStationary) {
+            qualifyingTouches += 1;
+        }
+    }
+
+    if (qualifyingTouches >= 3) {
+        [self armIfNeeded];
+    } else {
+        [self cancelHoldAndResetTrigger];
+    }
+}
+
+- (void)armIfNeeded {
+    if (self.triggered || self.holdTimer != nil) {
+        return;
+    }
+
+    if (![NSThread isMainThread]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self armIfNeeded];
+        });
+        return;
+    }
+
+    __weak typeof(self) weakSelf = self;
+    self.holdTimer = [NSTimer timerWithTimeInterval:PHThreeFingerHoldInterval
+                                              repeats:NO
+                                                block:^(__unused NSTimer *timer) {
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        [strongSelf handleHoldTimerFired];
+    }];
+
+    [[NSRunLoop mainRunLoop] addTimer:self.holdTimer
+                              forMode:NSRunLoopCommonModes];
+}
+
+- (void)handleHoldTimerFired {
+    self.holdTimer = nil;
+
+    if (self.triggered) {
+        return;
+    }
+
+    self.triggered = YES;
+    [[PHOverlayManager sharedManager] presentTestOverlayIfNeeded];
+}
+
+- (void)cancelHoldAndResetTrigger {
+    [self cancelTimer];
+    self.triggered = NO;
+}
+
+- (void)cancelTimer {
+    [self.holdTimer invalidate];
+    self.holdTimer = nil;
+}
+
+- (void)reset {
+    [self cancelHoldAndResetTrigger];
+}
+
+@end
